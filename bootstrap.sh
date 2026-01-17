ac02a01d3602922a423477bf6769de228fa155d7
mobbun <mmmmmmmmichellee@gmail.com>
2026-01-16 20:26:23 -0500

    ok well now it's actually react native app

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 # This script bootstraps a development environment by installing necessary packages
 # and setting up configuration files.
+#To run your project, navigate to the directory and run one of the following npm commands.
 
-npm ci
-npm run dev
+cd fridge
+npx install
+npx expo start # you need to use macOS to build the iOS project - use the Expo app if you need to do iOS development without a Mac
